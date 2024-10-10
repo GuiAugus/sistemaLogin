d5c6bc42825023add4a45e1936adb5ec1f1f5650
gui <augusto.g@outlook.com.br>
2024-10-10 20:15:11 -0300

    projeto iniciado

---
+++ sistemaLogin/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace sistemaLogin.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
