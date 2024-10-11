446f36c1e0f2953bd6bdfa586f35d6368215876a
gui <augusto.g@outlook.com.br>
2024-10-10 21:43:09 -0300

    configuracoes iniciais

---
+++ sistemaLogin/Models/AplicationUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace sistemaLogin.Models
+{
+    public class AplicationUser
+    {
+        public int id { get; set; }
+
+        public string user { get; set; }
+
+        public string password { get; set; }
+
+    }
+}
